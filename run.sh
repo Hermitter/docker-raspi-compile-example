fa11370b72c4cf5f477091a77820055c21d8dd4e
Carlos Chacin <chacin.carlos9798@gmail.com>
2019-05-24 16:36:16 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 ## Helpful Run Script ##
-IMG=pi3_img
+IMG=pi
 CONTAINER=pi_instance
+VOLUME=/INSERT_OUTPUT_FOLDER
 
 # Rebuild Docker Image
 docker build -t $IMG -f Dockerfile .
@@ -11,7 +12,7 @@ docker build -t $IMG -f Dockerfile .
 echo "Deleting old container..."
 docker rm -f $CONTAINER
 echo "Creating new container..."
-docker run --name=$CONTAINER -d -v /INSERT_OUTPUT_FOLDER:/volume $IMG
+docker run --name=$CONTAINER -d -v $VOLUME:/volume $IMG
 
 # View Live Logs
 docker logs -f $CONTAINER
